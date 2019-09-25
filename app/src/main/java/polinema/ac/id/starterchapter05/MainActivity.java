7fac877ee21165106d99714463e942ee1f380f82
Asus <alfarizqi92@gmail.com>
2019-09-25 16:41:18 +0700

    Dynamic Fragment dengan membuat 1 activities DynamicActivity dan tampilan pada xml

--- app/src/main/java/polinema/ac/id/starterchapter05/MainActivity.java
+++ app/src/main/java/polinema/ac/id/starterchapter05/MainActivity.java
@@ -6,6 +6,7 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 
+import polinema.ac.id.starterchapter05.activities.DynamicActivity;
 import polinema.ac.id.starterchapter05.activities.StaticActivity;
 import polinema.ac.id.starterchapter05.fragments.RedFragment;
 
@@ -23,12 +24,12 @@ public class MainActivity extends AppCompatActivity {
     }
 
     public void handlerClickDynamicFragment(View view) {
-        Intent intent = new Intent(this, RedFragment.class);
+        Intent intent = new Intent(this, DynamicActivity.class);
         startActivity(intent);
     }
 
     public void handlerClickPraktikumFragment(View view) {
-        Intent intent = new Intent(this, StaticActivity.class);
-        startActivity(intent);
+//        Intent intent = new Intent(this, StaticActivity.class);
+//        startActivity(intent);
     }
 }
