90e4f4a2f74e425531110d4694253e0e98a23d6d
Asus <alfarizqi92@gmail.com>
2019-09-27 22:04:21 +0700

    Latihan Load fragment dg menggunakan if else

--- app/src/main/java/polinema/ac/id/starterchapter05/activities/DynamicActivity.java
+++ app/src/main/java/polinema/ac/id/starterchapter05/activities/DynamicActivity.java
@@ -1,6 +1,7 @@
 package polinema.ac.id.starterchapter05.activities;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
 import androidx.fragment.app.FragmentTransaction;
 
 import android.os.Bundle;
@@ -19,17 +20,70 @@ public class DynamicActivity extends AppCompatActivity {
     }
 
     public void handlerClickLoadRedFragment(View view) {
+//        1
+        RedFragment redFragment = (RedFragment) getSupportFragmentManager().findFragmentByTag("RED_FRAGMENT");
         FragmentTransaction fragmentTransaction = getSupportFragmentManager().beginTransaction();
-        fragmentTransaction.replace(R.id.dynamic_fragment_placeholder,new RedFragment());
-        fragmentTransaction.addToBackStack(null);
-        fragmentTransaction.commit();
+        if(redFragment != null && redFragment.isVisible()){
+//            fragmentTransaction.commit();
+        }else{
+//            FragmentTransaction fragmentTransaction = getSupportFragmentManager().beginTransaction();
+//            fragmentTransaction.setTransition(FragmentTransaction.TRANSIT_FRAGMENT_OPEN);
+//            fragmentTransaction.setCustomAnimations(R.anim.enter_from_left,R.anim.exit_from_left,R.anim.enter_from_left,R.anim.exit_from_right);
+            fragmentTransaction.setCustomAnimations(R.anim.exit_from_right,R.anim.exit_from_left);
+            fragmentTransaction.replace(R.id.dynamic_fragment_placeholder,new RedFragment(),"RED_FRAGMENT");
+            fragmentTransaction.addToBackStack(null);
+            fragmentTransaction.commit();
+        }
+//        2
+//        FragmentTransaction fragmentTransaction = getSupportFragmentManager().beginTransaction();
+//        fragmentTransaction.setTransition(FragmentTransaction.TRANSIT_FRAGMENT_OPEN);
+//        fragmentTransaction.setCustomAnimations(R.anim.enter_from_left,R.anim.exit_from_left,R.anim.enter_from_left,R.anim.exit_from_right);
+//        fragmentTransaction.replace(R.id.dynamic_fragment_placeholder,new RedFragment(),"RED_FRAGMENT");
+//        fragmentTransaction.addToBackStack(null);
+//        fragmentTransaction.commit();
+
+//        3
+//        Fragment fragment = getSupportFragmentManager().findFragmentById(R.id.dynamic_fragment_placeholder);
+//        if(fragment == null || fragment instanceof BlueFragment){
+//            getSupportFragmentManager().beginTransaction()
+//                    .setCustomAnimations(R.anim.exit_from_right,R.anim.exit_from_left)
+//                    .replace(R.id.dynamic_fragment_placeholder,new RedFragment() , "RED_FRAGMENT")
+//                    .addToBackStack(null)
+//                    .commit();
+//        }
     }
 
     public void handlerClickLoadBlueFragment(View view) {
+//        1
+        BlueFragment blueFragment = (BlueFragment) getSupportFragmentManager().findFragmentByTag("BLUE_FRAGMENT");
         FragmentTransaction fragmentTransaction = getSupportFragmentManager().beginTransaction();
-        fragmentTransaction.replace(R.id.dynamic_fragment_placeholder,new BlueFragment());
-        fragmentTransaction.addToBackStack(null);
-        fragmentTransaction.commit();
+        if(blueFragment != null && blueFragment.isVisible()){
+//            fragmentTransaction.commit();
+        }else {
+//            FragmentTransaction fragmentTransaction = getSupportFragmentManager().beginTransaction();
+//            fragmentTransaction.setTransition(FragmentTransaction.TRANSIT_FRAGMENT_OPEN);
+//            fragmentTransaction.setCustomAnimations(R.anim.enter_from_left,R.anim.exit_from_left,R.anim.enter_from_left,R.anim.exit_from_right);
+            fragmentTransaction.setCustomAnimations(R.anim.enter_from_left,R.anim.enter_from_right);
+            fragmentTransaction.replace(R.id.dynamic_fragment_placeholder,new BlueFragment(),"BLUE_FRAGMENT");
+            fragmentTransaction.addToBackStack(null);
+            fragmentTransaction.commit();
+        }
 
+//        2
+//        FragmentTransaction fragmentTransaction = getSupportFragmentManager().beginTransaction();
+//        fragmentTransaction.setTransition(FragmentTransaction.TRANSIT_FRAGMENT_OPEN);
+//        fragmentTransaction.replace(R.id.dynamic_fragment_placeholder,new BlueFragment(),"BLUE_FRAGMENT");
+//        fragmentTransaction.addToBackStack(null);
+//        fragmentTransaction.commit();
+
+//        3
+//        Fragment fragment = getSupportFragmentManager().findFragmentByTag("BLUE_FRAGMENT");
+//        if(fragment == null ){
+//            getSupportFragmentManager().beginTransaction()
+//                    .setCustomAnimations(R.anim.enter_from_left,R.anim.enter_from_right)
+//                    .replace(R.id.dynamic_fragment_placeholder,new BlueFragment() , "BLUE_FRAGMENT")
+//                    .addToBackStack(null)
+//                    .commit();
+//        }
     }
 }
